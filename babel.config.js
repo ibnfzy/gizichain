ff8c91b4df635a3b0569bc77c06a27acd8ed0f42
Ibnu Alboneh <58937373+ibnfzy@users.noreply.github.com>
2025-10-24 23:04:42 +0800

    Fix nativewind Babel plugin usage for SDK 54

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,14 @@
+const nativewind = require('nativewind/babel');
+
 module.exports = function (api) {
   api.cache(true);
+
+  const nativewindConfig = nativewind();
+
   return {
     presets: [require.resolve('babel-preset-expo')],
     plugins: [
-      require.resolve('nativewind/babel'),
+      ...(nativewindConfig?.plugins ?? []),
       require.resolve('expo-router/babel'),
     ],
   };
